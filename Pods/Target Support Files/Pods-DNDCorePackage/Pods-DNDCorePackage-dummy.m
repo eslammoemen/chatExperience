d93936553f448b12f89048169bfeef4fceda7c63
eslammoemen <eslammoemen@icloud.com>
2023-12-18 14:30:44 +0200

    updated Integration Layer

--- Pods/Target Support Files/Pods-DNDCorePackage/Pods-DNDCorePackage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DNDCorePackage : NSObject
-@end
-@implementation PodsDummy_Pods_DNDCorePackage
-@end
--- Pods/Target Support Files/Pods-chatsModule/Pods-chatsModule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_chatsModule : NSObject
-@end
-@implementation PodsDummy_Pods_chatsModule
-@end
